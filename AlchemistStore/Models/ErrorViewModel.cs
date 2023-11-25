a02243d3fc3f801d48c548d242a532ae6ecbdf16
DarkLord <dfhrhfan12@yandex.ru>
2023-11-25 13:59:42 +0400

    Create mvc project

---
+++ AlchemistStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlchemistStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- AlchemistStore/Program.cs
+++ AlchemistStore/Program.cs
@@ -1,4 +1,4 @@
-namespace Studying._273.WebApp
+namespace AlchemistStore
 {
     public class Program
     {
@@ -7,14 +7,14 @@ namespace Studying._273.WebApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddRazorPages();
+            builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -26,7 +26,9 @@ namespace Studying._273.WebApp
 
             app.UseAuthorization();
 
-            app.MapRazorPages();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
