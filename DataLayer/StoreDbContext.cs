32d8ac52f7546719bf6cb03b05b3419c60f7720b
DarkLord <dfhrhfan12@yandex.ru>
2023-12-11 22:35:25 +0400

    Add StoreDbContext

---
+++ DataLayer/StoreDbContext.cs
@@ -0,0 +1,32 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class StoreDbContext : DbContext
+    {
+        public DbSet<DElement> Elements { get; set; } = null!;
+        public DbSet<DProduct> Products { get; set; } = null!;
+
+        public DbSet<DClient> Clients { get; set; } = null!;
+        public DbSet<DSale> Sales { get; set; } = null!;
+
+        public DbSet<DTransmutationRecipe> TransmutationRecipes { get; set; } = null!;
+        public DbSet<DTransmutation> Transmutations { get; set; } = null!;
+
+        public StoreDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp456.db");
+        }
+    }
+}
