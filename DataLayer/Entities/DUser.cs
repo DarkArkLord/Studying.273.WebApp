988a3c3387d7b883e3666f05fbcd3e19257fb707
DarkLord <dfhrhfan12@yandex.ru>
2023-12-22 22:30:18 +0400

    Upd user

--- DataLayer/Entities/DUser.cs
+++ DataLayer/Entities/DUser.cs
@@ -3,7 +3,8 @@
     public class DUser
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Login { get; set; }
+        public string PassHash { get; set; }
         public EUserType UserType { get; set; }
     }
 }
