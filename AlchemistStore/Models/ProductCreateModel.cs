29d4e75ef9b5d5c2f436d39897567b5655e05bed
DarkLord <dfhrhfan12@yandex.ru>
2023-12-23 03:34:29 +0400

    aaa

---
+++ AlchemistStore/Models/ProductCreateModel.cs
@@ -0,0 +1,11 @@
+﻿using DataLayer.Entities;
+using System.Collections.Generic;
+
+namespace AlchemistStore.Models
+{
+    public class ProductCreateModel
+    {
+        public IEnumerable<DElement> Elements { get; set; }
+        public string ErrorText { get; set; }
+    }
+}
