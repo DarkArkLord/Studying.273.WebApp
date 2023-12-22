6cf3bfe9d4ce6dc0aa44f7bf63ebe0d5d00313b9
DarkLord <dfhrhfan12@yandex.ru>
2023-12-23 03:37:05 +0400

    Kill them all

--- AlchemistStore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AlchemistStore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
