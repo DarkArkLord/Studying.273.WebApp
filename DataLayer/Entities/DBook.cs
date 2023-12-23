811b025908dd39a55ca27f3f71711a08066ba3d6
DarkLord <dfhrhfan12@yandex.ru>
2023-12-23 04:37:11 +0400

    Fix entity

--- DataLayer/Entities/DBook.cs
+++ DataLayer/Entities/DBook.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public DAuthor Author { get; set; }
-        public DBookSeries Series { get; set; }
+        public DBookSeries? Series { get; set; }
     }
 }
