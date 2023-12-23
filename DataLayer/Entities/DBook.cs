e741af7f59af55abf4983ace581e5a2c32d68388
DarkLord <dfhrhfan12@yandex.ru>
2023-12-23 05:03:13 +0400

    Upd entity

--- DataLayer/Entities/DBook.cs
+++ DataLayer/Entities/DBook.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DAuthor Author { get; set; }
+        public DAuthor? Author { get; set; }
         public DBookSeries? Series { get; set; }
     }
 }
