17561ead5c5a2c2c02353d6c4d64500a57f417ad
DarkLord <dfhrhfan12@yandex.ru>
2023-12-30 12:21:41 +0400

    Join http

--- WebApiUtils/DarkHttpClient.cs
+++ WebApiUtils/DarkHttpClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace WebApiUtils
 {
@@ -25,5 +26,41 @@ namespace WebApiUtils
         {
             client.Dispose();
         }
+
+        #region HttpRequestMessage
+        public class DarkHttpRequestMessage
+        {
+            private HttpClient client;
+            private HttpRequestMessage message;
+
+            public DarkHttpRequestMessage(HttpClient client)
+            {
+                this.client = client;
+                message = new HttpRequestMessage();
+            }
+
+            public DarkHttpRequestMessage SetMethod(HttpMethod method)
+            {
+                message.Method = method;
+                return this;
+            }
+
+            public DarkHttpRequestMessage SetUri(string uri)
+            {
+                message.RequestUri = new Uri(uri);
+                return this;
+            }
+
+            public HttpResponseMessage Send()
+            {
+                return client.Send(message);
+            }
+
+            public async Task<HttpResponseMessage> SendAsync()
+            {
+                return await client.SendAsync(message);
+            }
+        }
+        #endregion
     }
 }
