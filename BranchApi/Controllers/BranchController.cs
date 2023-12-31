fac3de4c72a0605640a49bf9164c2c0f8c48c0b9
DarkLord <dfhrhfan12@yandex.ru>
2023-12-31 14:27:28 +0400

    Add branch api

---
+++ BranchApi/Controllers/BranchController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApiUtils.BaseApi;
+
+namespace BranchApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BranchController : ApiWithNameController { }
+}
