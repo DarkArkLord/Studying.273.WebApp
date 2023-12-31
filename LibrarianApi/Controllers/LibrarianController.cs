73d778c7cb0f9f466855cd5e899348efd1574386
DarkLord <dfhrhfan12@yandex.ru>
2023-12-31 14:31:58 +0400

    Add libr api

---
+++ LibrarianApi/Controllers/LibrarianController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApiUtils.BaseApi;
+
+namespace LibrarianApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LibrarianController : ApiWithNameController { }
+}
