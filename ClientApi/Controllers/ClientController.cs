d50cfa3e14e6aaec54fe2401b9faa08f9138a82f
DarkLord <dfhrhfan12@yandex.ru>
2023-12-31 14:38:38 +0400

    Add client id

---
+++ ClientApi/Controllers/ClientController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApiUtils.BaseApi;
+
+namespace ClientApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ApiWithNameController { }
+}
